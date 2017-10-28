4e29fec6440c86eb0cc11f808802b3c2f0086259
Paul van Dam <pvandam@m-industries.com>
2017-10-28 21:08:16 +0200

    Formatting.

--- alan-fabric-mode.el
+++ alan-fabric-mode.el
@@ -39,158 +39,25 @@
 	  ("'[^']*?'" . font-lock-variable-name-face)
 	  ("\\(->\\|~>\\)\\s-+\\(stategroup\\|component\\|group\\|file\\|dictionary\\|command\\|matrix\\|reference\\|natural\\|integer\\|text\\)\\(\\s-+\\|$\\)" 2 font-lock-type-face)
 	  (,(regexp-opt '(
-					  "add" "branch" "ceil" "convert" "count" "division" "floor" "increment" "max" "min" "remainder" "subtract" "sum" "sumlist" "base" "diff"
-					  ))
+					  "add" "branch" "ceil" "convert" "count" "division" "floor" "increment" "max" "min" "remainder" "subtract" "sum" "sumlist" "base" "diff"))
 	   . font-lock-function-name-face)
-	  (,(regexp-opt '(
-					  "!"
-					  "#"
-					  "$"
-					  "%"
-					  "%^"
-					  "%}"
-					  "&"
-					  "("
-					  ")"
-					  "*"
-					  "*&"
-					  "*&#"
-					  "+"
-					  "+^"
-					  "-"
-					  "->"
-					  ".>"
-					  "~>"
-					  "."
-					  ".^"
-					  ":"
-					  "<"
-					  "<="
-					  "=="
-					  ">"
-					  ">="
-					  "?"
-					  "?^"
-					  "@"
-					  "add"
-					  "allow"
-					  "anonymous"
-					  "as"
-					  "branch"
-					  "ceil"
-					  "command"
-					  "component"
-					  "constrain"
-					  "contains"
-					  "convert"
-					  "count"
-					  "creation"
-					  "densematrix"
-					  "derived"
-					  "dictionary"
-					  "division"
-					  "dynamic"
-					  "empty"
-					  "execute"
-					  "external"
-					  "floor"
-					  "forward"
-					  "from"
-					  "group"
-					  "id"
-					  "implementation"
-					  "in"
-					  "increment"
-					  "integer"
-					  "interface"
-					  "lifetime"
-					  "link"
-					  "log"
-					  "map"
-					  "matrix"
-					  "max"
-					  "min"
-					  "mutation"
-					  "natural"
-					  "no"
-					  "now"
-					  "number"
-					  "numerical"
-					  "on"
-					  "ontimeout"
-					  "or"
-					  "ordered"
-					  "password"
-					  "path"
-					  "product"
-					  "property"
-					  "read"
-					  "reference"
-					  "remainder"
-					  "remove"
-					  "rename"
-					  "requires"
-					  "resolvable"
-					  "roles"
-					  "root"
-					  "set"
-					  "setto"
-					  "sparsematrix"
-					  "stategroup"
-					  "stopwatch"
-					  "subtract"
-					  "sum"
-					  "sumlist"
-					  "text"
-					  ".self"
-					  "time"
-					  "timer"
-					  "to"
-					  "types"
-					  "unconstrained"
-					  "update"
-					  "users"
-					  "with"
-					  "workfor"
-					  "yes"
-					  "new"
-					  "~"
-					  "}"
-					  "/"
-					  "{"
-					  "@?^"
-					  "@%^"
-					  "^"
-					  "`"
-					  "="
-					  "ignore"
-					  "file"
-					  "create"
-					  "delete"
-					  "created"
-					  "entry"
-					  "acyclic"
-					  "space"
-					  "TEMP"
-					  "TEMP2"
-					  "relative"
-					  "preceding"
-					  "base"
-					  "diff"
-					  "std"
-					  "deprecated"
-					  "zero"
-					  "one"
-					  "$^"
-					  "bidirectional"
-					  "<~"
-					  "invalidate"
-					  "referencer"
-					  "separator"
-					  "FIXME"
-					  ))
-	   . font-lock-builtin-face)
-	  )
+	  (,(regexp-opt '("!" "#" "$" "%" "%^" "%}" "&" "(" ")" "*" "*&" "*&#" "+" "+^" "-" "->" ".>"
+					  "~>" "." ".^" ":" "<" "<=" "==" ">" ">=" "?" "?^" "@" "add" "allow"
+					  "anonymous" "as" "branch" "ceil" "command" "component" "constrain" "contains"
+					  "convert" "count" "creation" "densematrix" "derived" "dictionary" "division"
+					  "dynamic" "empty" "execute" "external" "floor" "forward" "from" "group" "id"
+					  "implementation" "in" "increment" "integer" "interface" "lifetime" "link"
+					  "log" "map" "matrix" "max" "min" "mutation" "natural" "no" "now" "number"
+					  "numerical" "on" "ontimeout" "or" "ordered" "password" "path" "product"
+					  "property" "read" "reference" "remainder" "remove" "rename" "requires"
+					  "resolvable" "roles" "root" "set" "setto" "sparsematrix" "stategroup"
+					  "stopwatch" "subtract" "sum" "sumlist" "text" ".self" "time" "timer" "to"
+					  "types" "unconstrained" "update" "users" "with" "workfor" "yes" "new" "~" "}"
+					  "/" "{" "@?^" "@%^" "^" "`" "=" "ignore" "file" "create" "delete" "created"
+					  "entry" "acyclic" "space" "TEMP" "TEMP2" "relative" "preceding" "base" "diff"
+					  "std" "deprecated" "zero" "one" "$^" "bidirectional" "<~" "invalidate"
+					  "referencer" "separator" "FIXME"))
+	   . font-lock-builtin-face))
 	nil nil nil nil
 	(font-lock-syntactic-face-function . alan-font-lock-syntactic-face-function))
   "Highlighting for alan mode.")
@@ -206,45 +73,36 @@
 	(move-beginning-of-line 1)
 	(while (and (not (bobp))
 				(looking-at line-to-ignore-regex))
-	  (forward-line -1)
-	  )
+	  (forward-line -1))
 
 	(let ((start-indent (current-indentation))
 		  (curr-indent (current-indentation))
 		  (curr-point (point))
-		  (start-line-number (line-number-at-pos))
-
-		  )
+		  (start-line-number (line-number-at-pos)))
 	  (defvar new-point)
 	  (while (and (not (bobp))
 				  (> start-indent 0)
 				  (or (not (looking-at "\\s-*'[^']*?'"))
 					  (looking-at line-to-ignore-regex)
 					  (> (current-indentation) curr-indent)
-					  (<= start-indent (current-indentation))
-					  ))
+					  (<= start-indent (current-indentation))))
 		(forward-line -1)
 		(unless  (looking-at line-to-ignore-regex)
-		  (setq curr-indent (min curr-indent (current-indentation)))
-		  )
-		(setq new-point (point))
-		)
+		  (setq curr-indent (min curr-indent (current-indentation))))
+		(setq new-point (point)))
 	  (if
 		  (and
 		   (looking-at alan-parent-regexp)
-		   (not (equal start-line-number (line-number-at-pos)))
-		   )
-		  (match-beginning 1)
-		)
-	  )))
+		   (not (equal start-line-number (line-number-at-pos))))
+		  (match-beginning 1)))))
 
 (defun alan-goto-parent ()
   "Goto the parent of this property."
   (interactive)
   (let ((parent-position (alan-has-parent)))
 	(when (alan-has-parent)
-		(push-mark (point) t)
-		(goto-char parent-position))))
+	  (push-mark (point) t)
+	  (goto-char parent-position))))
 
 (defun alan-copy-path-to-clipboard ()
   "Copies the path to the clipboard.
@@ -252,10 +110,7 @@ Usefull for writing long paths."
   (interactive)
   (let ((path (alan-path)))
 	(when path
-	  (kill-new path)
-	  )
-	)
-)
+	  (kill-new path))))
 
 (defun alan-path ()
   "Gives the location as a path of where you are in a file.
@@ -264,14 +119,10 @@ E.g. 'views' . 'queries' . 'context' . 'candidates' . 'of'"
   (save-excursion
 	(while (setq has-parent (alan-has-parent))
 	  (goto-char has-parent)
-	  (add-to-list 'path-list (match-string 1))
-	  )
-	)
+	  (add-to-list 'path-list (match-string 1))))
   (if (> (length path-list) 0)
 	  (mapconcat 'identity path-list " . ")
-	""
-	)
-  )
+	""))
 
 (defun alan-fabric-mode-indent-line ()
   "Indentation based mostly on parens.
@@ -283,16 +134,14 @@ indentations.  But from then on this works fine."
 	  (beginning-of-line)
 	  (if (bobp)
 		  ;;at the beginning indent to 0
-		  (indent-line-to 0)
-		)
+		  (indent-line-to 0))
 	  ;; take the current indentation of the enclosing expression
 	  (let ((parent-position (nth 1 (syntax-ppss)))
 			(previous-line-indentation (and
 										(not (bobp))
 										(save-excursion
 										  (forward-line -1)
-										  (current-indentation))))
-			)
+										  (current-indentation)))))
 		(cond
 		 (parent-position
 		  (let ((parent-indent
@@ -303,25 +152,18 @@ indentations.  But from then on this works fine."
 				(setq new-indent parent-indent)
 			  (setq new-indent ( + parent-indent default-tab-width)))))
 		 (previous-line-indentation
-		  (setq new-indent previous-line-indentation)
-		  )
-		 )
+		  (setq new-indent previous-line-indentation)))
 		;; check for single block and add a level of indentation.
 		(save-excursion
 		  (back-to-indentation)
 		  (if (and (looking-at "\\s(")
-			  (eq
-			   (line-number-at-pos)
-			   (progn
-				 (forward-sexp)
-				 (line-number-at-pos)
-				 )
-			   ))
+				   (eq
+					(line-number-at-pos)
+					(progn
+					  (forward-sexp)
+					  (line-number-at-pos))))
 			  (setq new-indent (min (if previous-line-indentation (+ previous-line-indentation default-tab-width) default-tab-width )
-									(+ new-indent default-tab-width)))
-			)
-		  )
-		))
+									(+ new-indent default-tab-width)))))))
 	(when new-indent
 	  (indent-line-to new-indent))))
 
@@ -334,22 +176,19 @@ indentations.  But from then on this works fine."
    (error line-start (file-name) ":" line ":" column ": error:" (optional " " (one-or-more digit) ":" (one-or-more digit)) "\n"
    		  (message (one-or-more " " (one-or-more not-newline) "\n")))
    (warning line-start (file-name) ":" line ":" column ": warning:" (optional " " (one-or-more digit) ":" (one-or-more digit)) "\n"
-   		  (message (one-or-more " " (one-or-more not-newline) "\n")))
-   )
-  :modes (alan-fabric-mode)
-)
+			(message (one-or-more " " (one-or-more not-newline) "\n"))))
+  :modes (alan-fabric-mode))
 (add-to-list 'flycheck-checkers 'alan-fabric-validator)
 
 (defun alan-font-lock-syntactic-face-function (state)
   "Do not fontify single quoted strings.
-This function gets a STATE that is the result of 'parse-partial-sexp'."
+This function gets a STATE that is the result of `parse-partial-sexp'."
   (if (nth 3 state)
       (let ((startpos (nth 8 state)))
         (if (eq (char-after startpos) ?')
             ;; This is not a string, but an identifier.
             nil
-		  font-lock-string-face
-          ))
+		  font-lock-string-face))
     font-lock-comment-face))
 
 (defun alan-schedule-timer ()
@@ -357,13 +196,10 @@ This function gets a STATE that is the result of 'parse-partial-sexp'."
   (when (timerp alan-update-headline-timer)
 	(cancel-timer alan-update-headline-timer))
   (setq alan-update-headline-timer (run-with-idle-timer
-   0.5 nil
-   (lambda ()
-	 (setq header-line-format (format " %s  " (alan-path)))
-	 (force-mode-line-update)
-	 )
-   ))
-  )
+									0.5 nil
+									(lambda ()
+									  (setq header-line-format (format " %s  " (alan-path)))
+									  (force-mode-line-update)))))
 
 (define-derived-mode alan-fabric-mode prog-mode "fabric"
   "Major mode for editing m-industries alan fabric files."
@@ -376,8 +212,7 @@ This function gets a STATE that is the result of 'parse-partial-sexp'."
   (set (make-local-variable 'compilation-error-screen-columns) nil)
   (set (make-local-variable 'indent-line-function) 'alan-fabric-mode-indent-line)
   (add-hook 'post-command-hook #'alan-schedule-timer nil t)
-  (setq header-line-format "")
-  )
+  (setq header-line-format ""))
 
 (provide 'alan-fabric)
 
